1e4a7908113fa402243349e3cea77286abe9f9d7
Matthew Leroe <matthewleroe@Matthews-MBP.fios-router.home>
2020-06-15 15:52:13 -0400

    Screen Practice Next Project

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // on click, change menu icon to cross
 // on click, change cross to menu 
 
-
 let bgr = document.getElementById("burger");
 bgr.addEventListener('click', function() {
     console.log("Yum");
@@ -46,4 +45,30 @@ node.removeChild
 https://developer.mozilla.org/en-US/docs/Web/API/Node/removeChild
 
 */
+
+
+// onLoad, present a box that can be removed with a click in the upper right corner
+// 0. Greeting / Terms 
+// i. LogIn to Google, Facebook, or Phone
+// ii. Trouble Logging in Link
+// 
+
+// opacity 0.5
+
+// z-index over all others; opacity 0.0 vs. 0.5 in others
+// position: absolute? removed from natural flow ... put at html bottom, but moved to top
+
+// let ticket = document.querySelector('.ticket');
+// ticket.addEventListener('load', function(e){
+//     e.preventDefault;
+//     ticket.style.visibility = "visible";
+// });
+
+// // what is the parent node of ticket? 
+// let ticket_btn = document.querySelector('#ticket_btn').addEventListener('click', function(e){
+//     console.log(ticket.parentNode)
+//     let con = document.querySelector('.container');
+//     con.removeChild(ticket);
+//     // ticket.style.visibility = "hidden";
+// });
 
